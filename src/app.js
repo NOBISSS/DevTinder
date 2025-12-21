4c358d1e749b3e801217239528710a95e90a944d
NOBISSS <parthchauhan25125.com>
2025-12-22 00:13:07 +0530

    ROUTING AND MIDDLEWARE

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express=require("express");
 
 const app=express();
 
-app.use((req,res)=>{
-    res.send("Hello from the server");
-})
+app.use("/user/:id",
+    (req,res,next)=>{
+        console.log("REQUEST PARAMS:",req.params);
+        next();
+        res.send("Response from 1st one");
+    },
+    (req,res)=>{
+        console.log("REQUEST PARAMS2nd:",req.params);
+        res.send("Response from 2nd one");
+    })
 
 app.listen(3000,(req,res)=>{
     console.log("Server is started");
