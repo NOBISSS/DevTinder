44e56f3a0e02a92600dbe2bc703ef4ee0314b4d3
NOBISSS <parthchauhan25125.com>
2025-12-22 12:27:18 +0530

    Data Sanitization and Validatation

--- src/app.js
+++ src/app.js
@@ -3,18 +3,79 @@ const { adminAuth, userAuth } = require("./middlewares/auth");
 const connectDB=require("./config/database");
 const User=require("./models/user");
 const app=express();
-
+app.use(express.json());
+//Create new User in DB
 app.post("/signup",async (req,res)=>{
-    //Creating a new instance of the user model
-    const user=new User({
-        firstName:"Parth",
-        lastName:"Chauhan",
-        emailId:"abc@gmail.com",
-        password:"abc@123",
-        gender:"male"
-    }); 
-    await user.save();
-    res.send("User Added Successfully");
+    console.log(req.body);
+    const user=new User(req.body);
+    try{
+        await user.save();
+    }catch(error){
+        console.log(error);
+        res.send(error.message);
+    }
+    res.send("Done");
+    
+})
+
+//Find One User
+app.get("/user",async(req,res)=>{
+    const user=await User.find({emailId:req.body.emailId})
+    if(user.length===0){ res.status(404).send("User not Found")}else{
+    res.send(user);
+    }
+})
+
+app.put("/user",async(req,res)=>{
+    let email=req.body.emailId;
+    email="XYZ@gmail.com";
+    const user=await User.findOneAndUpdate({emailId:req.body.emailId},{emailId:email});
+    if(!user) return res.status(404).send("User not found");
+    res.send("User Data Updated");
+})
+// app.patch("/user",async(req,res)=>{
+//     let email=req.body.emailId;
+//     email="XYZ@gmail.com";
+//     const user=await User.findOneAndUpdate({emailId:req.body.emailId},{emailId:email});
+//     if(!user) return res.status(404).send("User not found");
+//     res.send("User Data Updated");
+// })
+
+app.patch("/user",async(req,res)=>{
+    const data=req.body;
+    try{
+    const ALLOWED_UPDATES=[
+        "photourl",
+        "userId",
+        "about",
+        "gender",
+        "age",
+        "skills"
+    ]
+
+    const isUpdateAllowed=Object.keys(data).every(k=>ALLOWED_UPDATES.includes(k));
+    if(!isUpdateAllowed){
+        res.status(400).send("Only Update Allowed Fields");
+    }
+    email="XYZ@gmail.com";
+    const user=await User.findOneAndUpdate({_id:data.userId},data,{
+        returnDocument:"before",
+        runValidators:true
+    });
+    console.log(user);
+    if(!user) return res.status(404).send("User not found");
+    res.send("User Data Updated");
+    }catch(error){
+        console.log("Error Occured While Updating User")
+        throw new Error("Failed to Update");
+    }
+})
+
+app.use((err,req,res,next)=>{
+    if(err){
+        console.log("Error :",err);
+    }
+    next();
 })
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,33 @@
 const mongoose=require("mongoose");
-
+const validator=require("validator");
 const userSchema=new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
     },
     lastName:{
         type:String,
     },
     emailId:{
         type:String,
-    },
+        lowercase:true,
+        unique:true,
+        required:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid Email Address"+value)
+            }
+        }
+    },  
     password:{
         type:String,
+        required:true,
+         validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter Strong password"+value)
+            }
+        }
     },
     age:{
         type:Number,
@@ -19,6 +35,21 @@ const userSchema=new mongoose.Schema({
     gender:{
         type:String,
     },
+    photoUrl:{
+        type:String,
+         validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid URL")
+            }
+        }
+    },
+    about:{
+        type:String,
+        default:"This is a Default Description"
+    },
+    skills:{
+       type:[String], 
+    }
 })
 
 module.exports=mongoose.model("User",userSchema);
