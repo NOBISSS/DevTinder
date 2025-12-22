92814cde5ec13500a9c4927f4810f853ba5063fd
NOBISSS <parthchauhan25125.com>
2025-12-22 15:15:47 +0530

    Cookies and Token Implementation

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const User=require("./models/user");
 const { validateSignUpData } = require("./utils/validation");
 const app=express();
 const bcrypt=require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt=require("jsonwebtoken");
 app.use(express.json());
+app.use(cookieParser());
 //Create new User in DB
 app.post("/signup",async (req,res)=>{
     try{
@@ -43,6 +46,11 @@ app.post("/login",async(req,res)=>{
         console.log(user);
         const isPasswordValid=await bcrypt.compare(password,user.password);
         if(isPasswordValid){
+            //create a JWT TOKEN
+            const token=jwt.sign({_id:user._id},"DEV@TINDER$790",{expiresIn:"1d"})
+            console.log(token)
+            //Add the Token into cookie
+            res.cookie("token",token);
             res.send("Password is correct || Logged Successfully");
         }else{
             throw new Error("Please Enter Correct Password");
@@ -53,64 +61,25 @@ app.post("/login",async(req,res)=>{
     }
 })
 
-//Find One User
-app.get("/user",async(req,res)=>{
-    const user=await User.find({emailId:req.body.emailId})
-    if(user.length===0){ res.status(404).send("User not Found")}else{
-    res.send(user);
-    }
-})
 
-app.put("/user",async(req,res)=>{
-    let email=req.body.emailId;
-    email="XYZ@gmail.com";
-    const user=await User.findOneAndUpdate({emailId:req.body.emailId},{emailId:email});
-    if(!user) return res.status(404).send("User not found");
-    res.send("User Data Updated");
-})
-// app.patch("/user",async(req,res)=>{
-//     let email=req.body.emailId;
-//     email="XYZ@gmail.com";
-//     const user=await User.findOneAndUpdate({emailId:req.body.emailId},{emailId:email});
-//     if(!user) return res.status(404).send("User not found");
-//     res.send("User Data Updated");
-// })
 
-app.patch("/user",async(req,res)=>{
-    const data=req.body;
+app.get("/profile",userAuth,async(req,res)=>{
     try{
-    const ALLOWED_UPDATES=[
-        "photourl",
-        "userId",
-        "about",
-        "gender",
-        "age",
-        "skills"
-    ]
-
-    const isUpdateAllowed=Object.keys(data).every(k=>ALLOWED_UPDATES.includes(k));
-    if(!isUpdateAllowed){
-        res.status(400).send("Only Update Allowed Fields");
-    }
-    email="XYZ@gmail.com";
-    const user=await User.findOneAndUpdate({_id:data.userId},data,{
-        returnDocument:"before",
-        runValidators:true
-    });
-    console.log(user);
-    if(!user) return res.status(404).send("User not found");
-    res.send("User Data Updated");
+        const user=req.user;
+        res.send("USER: "+user);
     }catch(error){
-        console.log("Error Occured While Updating User")
-        throw new Error("Failed to Update");
+        console.log(error.message);
+        res.send("Invalid Cookie");
     }
 })
 
-app.use((err,req,res,next)=>{
-    if(err){
-        console.log("Error :",err);
+app.post("/sendconnectionrequest",userAuth,async(req,res)=>{
+    try{
+        console.log("SEND CONNECTION");
+        res.send("CONNECTION REQUEST SENT BY:"+req.user.firstName);
+    }catch(error){
+        res.send("ERROR:"+error.message);
     }
-    next();
 })
 
 connectDB()
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,3 +1,5 @@
+const jwt=require("jsonwebtoken");
+const User = require("../models/user");
 const adminAuth=(req,res,next)=>{
     console.log("ADMIN MIDDLEWARE");
     const token="XYZ";
@@ -8,13 +10,22 @@ const adminAuth=(req,res,next)=>{
     }
 }
 
-const userAuth=(req,res,next)=>{
-    console.log("USER MIDDLEWARE");
-    const token="XYZ";
-    if(token==="XYZ"){
-        next();
-    }else{
-        res.status(401).send("Not Authorized");
+const userAuth=async(req,res,next)=>{
+    try{
+    const {token}=req.cookies;
+    if(!token){
+        throw new Error("Token is not Valid");
+    }
+    const decoded=jwt.verify(token,"DEV@TINDER$790");
+    const user=await User.findById(decoded);
+    if(!user){
+        throw new Error("Not Found");
+    }
+    req.user=user;
+    next();
+    }catch(error){
+        console.log(error.message);
+        res.status(400).send("Error: "+error.message);
     }
 }
 
